bbeb4bc42cb6619a940a33cf3e32a20cb9a2478f
Harry Harpham <hwh33@cornell.edu>
2015-03-06 14:14:15 -0500

    Start implement threading

--- primality/primality.go
+++ primality/primality.go
@@ -8,13 +8,48 @@ package primality
 
 import "math"
 
-func IsPrime(n int) bool {
+func IsPrime(n, numThreads int) bool {
 	return naivePrimalityTest(n)
 }
 
+func naiveThreadedTest(n, numThreads int) bool {
+	// Test for trivial cases.
+	if n < 4 || divides(2, n) {
+		return false
+	}
+
+	// Now we test every odd number from 3 to n/2. We divide the work up
+	// between numThreads threads. It will end up slightly uneven in most
+	// cases, but the difference in workload is small.
+	var numJobs int
+	if (n/2)-3 >= 0 {
+		numJobs = (((n / 2) - 3) / 2) + 1
+	} else {
+		numJobs = 1
+	}
+
+	// In this case, it's not worth the overhead.
+	if numJobs < (numThreads * 2) {
+		return naivePrimalityTest(n)
+	}
+
+	jobsPerThread := numJobs / numThreads
+
+	// double check this
+	a := 3
+	b := a + (jobsPerThread * 2) - 2
+	for b < (n / 2) {
+		go factorExistsInRange(a, b, n)
+		a = b + 2
+		b = a + (jobsPerThread * 2) - 2
+	}
+	// get the rest
+	go factorExistsInRange(a, (n / 2), n)
+}
+
 func naivePrimalityTest(n int) bool {
-	// Test if n is even.
-	if divides(2, n) {
+	// Test for trivial cases.
+	if n < 4 || divides(2, n) {
 		return false
 	}
 
@@ -27,6 +62,16 @@ func naivePrimalityTest(n int) bool {
 
 	// At this point, it must be prime.
 	return true
+}
+
+/* Check if any odd numbers from a to b divide n. */
+func factorExistsInRange(a, b, n int) bool {
+	for i := a; i <= b; i += 2 {
+		if divides(i, n) {
+			return true
+		}
+	}
+	return false
 }
 
 /* Returns true if x divides y. */
